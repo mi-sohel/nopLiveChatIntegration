7150c03e8277464b9f884eb802999b0ae379932d
mi-sohel <misohel@hotmail.com>
2024-11-14 14:14:07 +0600

    Add support 4.70.x

---
+++ Nop.Plugin.Widgets.BsLiveChat/Nop.Plugin.Widgets.BsLiveChat/BsLiveChatSettings.cs
@@ -0,0 +1,10 @@
+﻿
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Widgets.BsLiveChat
+{
+    public class BsLiveChatSettings : ISettings
+    {
+        public string TrackingScript { get; set; }
+    }
+}
---
+++ Nop.Plugin.Widgets.BsLiveChat/Nop.Plugin.Widgets.BsLiveChat/Components/WidgetsBsLiveChatViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.BsLiveChat.Components
+{
+    [ViewComponent(Name = "WidgetsBsLiveChat")]
+    public class WidgetsBsLiveChatViewComponent : NopViewComponent
+    {
+        private readonly BsLiveChatSettings _bsLivesetting;
+
+        public WidgetsBsLiveChatViewComponent(BsLiveChatSettings bsLivesetting)
+        {
+            _bsLivesetting = bsLivesetting;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        {
+             
+           // return   new HtmlContentViewComponentResult(new HtmlString(_bsLivesetting.TrackingScript ?? string.Empty));
+            return View("~/Plugins/Widgets.BsLiveChat/Views/WidgetsBsLiveChat/PublicInfo.cshtml", _bsLivesetting.TrackingScript);
+        }
+
+        
+
+        
+    }
+}
